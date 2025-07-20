e0d890b710b8a12692294f1213173afe976c97ac
HaggardNova <in4freedom@tutanota.com>
2025-07-20 11:08:39 +0100

    initial

--- server.js
+++ server.js
@@ -50,17 +50,8 @@ app.use(limiter);
 // CORS configuration
 app.use(
   cors({
-    origin: "*",
+    origin: true,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "X-Requested-With",
-      "authentication",
-    ],
-    preflightContinue: true,
-    optionsSuccessStatus: 204,
   })
 );
 
