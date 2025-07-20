cc580f1bb74737a09af2c4b570e69cbe059e6ae4
HaggardNova <in4freedom@tutanota.com>
2025-07-20 11:31:21 +0100

    initial

--- server.js
+++ server.js
@@ -88,6 +88,20 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  next();
+});
+
 // Health check endpoint
 app.get('/health', (req, res) => {
   res.json({
