7d17bb86e0c1d7cdeb200261bcafe7bb56474c98
HaggardNova <in4freedom@tutanota.com>
2025-07-20 11:34:27 +0100

    initial

--- server.js
+++ server.js
@@ -63,22 +63,23 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 
 // Request logging middleware
+/*
 app.use((req, res, next) => {
   const timestamp = new Date().toISOString();
   console.log(`${timestamp} - ${req.method} ${req.path} - IP: ${req.ip}`);
-  //res.header("Access-Control-Allow-Credentials", true);
-  //res.header("Access-Control-Allow-Origin", req.headers.origin);
-  /*res.header(
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
     "Access-Control-Allow-Methods",
     "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
   );
   res.header(
     "Access-Control-Allow-Headers",
     "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  );*/
+  );
   next();
 });
-
+*/
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", req.headers.origin);
